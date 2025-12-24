13fb88d1aa454f1de01f3a8f6f6fcb8d1e1da018
Elian <elian_fl@hotmail.com>
2025-12-24 19:24:16 +0100

    refactor components, custom colors, translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      allowedHosts: [".lhr.life"],
+    },
   },
   i18n: {
     defaultLocale: "es",
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,20 +15,29 @@ export const languages = {
 
 export const ui = {
   es: {
+    //___HEADER
     "nav.about": "Sobre mí",
     "nav.projects": "Proyectos",
     "nav.contact": "Contacto",
 
+    //___FOOTER
+    footer: "Elián Fernández. Todos los derechos reservados.",
+    "footer.availability": "Disponible para nuevos proyectos.",
+
+    //___PROFILE DESCRIPTION
     "profileDescription.greeting": "Hola, soy Elián",
     "profileDescription.description": "Construyo sitios web y aplicaciones...",
     "profileDescription.currentFocus":
       "Fuera del trabajo, probablemente me encuentres explorando nuevas tecnologías o trabajando en proyectos paralelos. Si tienes un proyecto en mente, ¡no dudes en contactarme!",
 
+    //___EXPERIENCE SECTION
     experience: "Experiencia laboral y formación",
     "experience.currently": "Actualmente...",
+
     // LMC: LetMeCharge
     "experience.LMC.title": "Desarrollador Backend",
     "experience.LMC.company": "LetMeCharge",
+    "experience.LMC.date": "Julio 2024 - Presente",
     "experience.LMC.description":
       "Desarrollo de aplicaciones web y APIs utilizando NestJs, con despliegue en AWS.",
 
@@ -45,24 +54,41 @@ export const ui = {
     "experience.monlau.date": "Septiembre 2022 - Junio 2024",
     "experience.monlau.description":
       "Especialización en desarrollo de aplicaciones web.",
+
+    //_____PROJECTS SECTION
+    projects: "Proyectos",
+    "projects.code": "Código",
+    "projects.liveDemo": "Demo en vivo",
+
+    //_____TECH STACK SECTION
+    techStack: "Tecnologías",
   },
 
   en: {
+    //___HEADER
     "nav.about": "About",
     "nav.projects": "Projects",
     "nav.contact": "Contact",
 
+    //___FOOTER
+    footer: "Elián Fernández. All rights reserved.",
+    "footer.availability": "Available for new projects.",
+
+    //___PROFILE DESCRIPTION
     "profileDescription.greeting": "Hello, I'm Elián",
     "profileDescription.description":
       "I build websites and web applications across different industries, using amodular design approach to keep things simple and scalable.",
     "profileDescription.currentFocus":
       "Outside of work, you will probably find me exploring new technologies or working on side projects. If you have a project in mind, feel free to reach out!",
 
+    //___EXPERIENCE SECTION
     experience: "Work Experience and Education",
     "experience.currently": "Currently...",
+
     // LMC: LetMeCharge
     "experience.LMC.title": "Backend Developer",
     "experience.LMC.company": "LetMeCharge",
+    "experience.LMC.date": "July 2024 - Present",
     "experience.LMC.description":
       "Developing web applications and APIs using NestJs, deployed on AWS.",
 
@@ -79,25 +105,41 @@ export const ui = {
     "experience.monlau.date": "September 2022 - June 2024",
     "experience.monlau.description":
       "Especialización en desarrollo de aplicaciones web.",
+
+    //_____PROJECTS SECTION
+    projects: "Projects",
+    "projects.code": "Code",
+    "projects.liveDemo": "Live Demo",
+
+    //_____TECH STACK SECTION
+    techStack: "Tech Stack",
   },
 
   cat: {
+    //___HEADER
     "nav.about": "Sobre mi",
     "nav.projects": "Projectes",
     "nav.contact": "Contacte",
 
+    //___FOOTER
+    footer: "Elián Fernández. Tots els drets reservats.",
+    "footer.availability": "Disponible per a nous projectes.",
+
+    //___PROFILE DESCRIPTION
     "profileDescription.greeting": "Hola, sóc l'Elián",
     "profileDescription.description":
       "Construeixo llocs web i aplicacions web...",
     "profileDescription.currentFocus":
       "Fora de la feina, probablement em trobaràs explorant noves tecnologies o treballant en projectes paral·lels. Si tens un projecte en ment, no dubtis a contactar!",
 
+    //___EXPERIENCE SECTION
     experience: "Experiència laboral i formació",
     "experience.currently": "Actualment...",
 
     // LMC: LetMeCharge
     "experience.LMC.title": "Desenvolupador Backend",
     "experience.LMC.company": "LetMeCharge",
+    "experience.LMC.date": "Juliol 2024 - Present",
     "experience.LMC.description":
       "Desenvolupament d'aplicacions web i APIs utilitzant NestJs, desplegades a AWS.",
 
@@ -114,5 +156,13 @@ export const ui = {
     "experience.monlau.date": "Setembre 2022 - Juny 2024",
     "experience.monlau.description":
       "Especialització en desenvolupament d'aplicacions web.",
+
+    //_____PROJECTS SECTION
+    projects: "Projectes",
+    "projects.code": "Codi",
+    "projects.liveDemo": "Demo en viu",
+
+    //_____TECH STACK SECTION
+    techStack: "Tecnologies",
   },
 } as const;
