9f877ac59329ac313f22ea47216ddcb875c960ab
Elian <elian_fl@hotmail.com>
2025-12-24 02:59:21 +0100

    i18n, new components

---
+++ src/i18n/ui.ts
@@ -0,0 +1,118 @@
+export const languages = {
+  es: {
+    label: "Español",
+    flag: "es",
+  },
+  en: {
+    label: "English",
+    flag: "gb",
+  },
+  cat: {
+    label: "Català",
+    flag: "cat",
+  },
+};
+
+export const ui = {
+  es: {
+    "nav.about": "Sobre mí",
+    "nav.projects": "Proyectos",
+    "nav.contact": "Contacto",
+
+    "profileDescription.greeting": "Hola, soy Elián",
+    "profileDescription.description": "Construyo sitios web y aplicaciones...",
+    "profileDescription.currentFocus":
+      "Fuera del trabajo, probablemente me encuentres explorando nuevas tecnologías o trabajando en proyectos paralelos. Si tienes un proyecto en mente, ¡no dudes en contactarme!",
+
+    experience: "Experiencia laboral y formación",
+    "experience.currently": "Actualmente...",
+    // LMC: LetMeCharge
+    "experience.LMC.title": "Desarrollador Backend",
+    "experience.LMC.company": "LetMeCharge",
+    "experience.LMC.description":
+      "Desarrollo de aplicaciones web y APIs utilizando NestJs, con despliegue en AWS.",
+
+    // Prácticas
+    "experience.internship.title": "Desarrollador Web Junior",
+    "experience.internship.company": "Videologic Analytics",
+    "experience.internship.date": "Octubre 2023 - Febrero 2024",
+    "experience.internship.description":
+      "Desarrollo de sitios web y mantenimiento utilizando HTML, CSS y JavaScript.",
+
+    // Monlau
+    "experience.monlau.title": "Grado Superior en DAW",
+    "experience.monlau.company": "Centro de Estudios Monlau",
+    "experience.monlau.date": "Septiembre 2022 - Junio 2024",
+    "experience.monlau.description":
+      "Especialización en desarrollo de aplicaciones web.",
+  },
+
+  en: {
+    "nav.about": "About",
+    "nav.projects": "Projects",
+    "nav.contact": "Contact",
+
+    "profileDescription.greeting": "Hello, I'm Elián",
+    "profileDescription.description":
+      "I build websites and web applications across different industries, using amodular design approach to keep things simple and scalable.",
+    "profileDescription.currentFocus":
+      "Outside of work, you will probably find me exploring new technologies or working on side projects. If you have a project in mind, feel free to reach out!",
+
+    experience: "Work Experience and Education",
+    "experience.currently": "Currently...",
+    // LMC: LetMeCharge
+    "experience.LMC.title": "Backend Developer",
+    "experience.LMC.company": "LetMeCharge",
+    "experience.LMC.description":
+      "Developing web applications and APIs using NestJs, deployed on AWS.",
+
+    // Prácticas
+    "experience.internship.title": "Junior Web Developer",
+    "experience.internship.company": "Videologic Analytics",
+    "experience.internship.date": "October 2023 - February 2024",
+    "experience.internship.description":
+      "Developed and maintained websites using HTML, CSS, and JavaScript.",
+
+    // Monlau
+    "experience.monlau.title": "Grado Superior en DAW",
+    "experience.monlau.company": "Centro de Estudios Monlau",
+    "experience.monlau.date": "September 2022 - June 2024",
+    "experience.monlau.description":
+      "Especialización en desarrollo de aplicaciones web.",
+  },
+
+  cat: {
+    "nav.about": "Sobre mi",
+    "nav.projects": "Projectes",
+    "nav.contact": "Contacte",
+
+    "profileDescription.greeting": "Hola, sóc l'Elián",
+    "profileDescription.description":
+      "Construeixo llocs web i aplicacions web...",
+    "profileDescription.currentFocus":
+      "Fora de la feina, probablement em trobaràs explorant noves tecnologies o treballant en projectes paral·lels. Si tens un projecte en ment, no dubtis a contactar!",
+
+    experience: "Experiència laboral i formació",
+    "experience.currently": "Actualment...",
+
+    // LMC: LetMeCharge
+    "experience.LMC.title": "Desenvolupador Backend",
+    "experience.LMC.company": "LetMeCharge",
+    "experience.LMC.description":
+      "Desenvolupament d'aplicacions web i APIs utilitzant NestJs, desplegades a AWS.",
+
+    // Prácticas
+    "experience.internship.title": "Desenvolupador Web Junior",
+    "experience.internship.company": "Videologic Analytics",
+    "experience.internship.date": "Octubre 2023 - Febrer 2024",
+    "experience.internship.description":
+      "Desenvolupament i manteniment de llocs web utilitzant HTML, CSS i JavaScript.",
+
+    // Monlau
+    "experience.monlau.title": "Grau Superior en DAW",
+    "experience.monlau.company": "Centre d'Estudis Monlau",
+    "experience.monlau.date": "Setembre 2022 - Juny 2024",
+    "experience.monlau.description":
+      "Especialització en desenvolupament d'aplicacions web.",
+  },
+} as const;
